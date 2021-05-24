a7f20c576b655c16321b5d2ff37bd301776b5668
Allingham <christianallingham@gmail.com>
2021-05-24 20:57:12 +0200

    first

---
+++ BlazorMovie/Model/MovieDBResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorMovie.Model
+{
+    public class MovieDBResponse
+    {
+        public int page { get; set; }
+        public ObservableCollection<MovieItem> results { get; set; }
+    }
+}
